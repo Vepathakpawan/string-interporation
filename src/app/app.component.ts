f8c07251808551d224d57f80aab74e50f890b324
Vepathakpawan <pawanved007@gmail.com>
2021-03-27 02:08:12 -0700

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'stirngInterpolation';
+    name = 'Pawan';
+    email = 'pawanved007@gmail.com';
+    contact = '8379079890';
+    public a = 100;
+    public b = 20;
+    }
+
